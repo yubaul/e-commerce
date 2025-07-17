8ea791dcc062f3ef32b43e0e9e4a15901cc536c7
Baul Yu <qkdnfdl1478@gmail.com>
2025-07-17 21:14:03 +0900

    [MOCK] 잔액 충전 요청 객체 구조 변경

--- src/main/java/kr/baul/server/mock/account/MockAccountApiController.java
+++ src/main/java/kr/baul/server/mock/account/MockAccountApiController.java
@@ -2,9 +2,12 @@ package kr.baul.server.mock.account;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import kr.baul.server.response.CommonResponse;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "MockAccount", description = "계좌 Mock API (잔액 충전 / 조회)")
@@ -18,6 +21,24 @@ public class MockAccountApiController {
     public CommonResponse charge(@RequestBody MockAccountChargeRequest request){
         var result = MockChargeBalance.dummy();
         return CommonResponse.success(result);
+    }
+
+    @Getter
+    @Setter
+    public static class MockAccountChargeRequest {
+
+        @Schema(description = "사용자 ID", example = "1", requiredMode = Schema.RequiredMode.REQUIRED)
+        private Long userId;
+
+        @Schema(description = "충전 금액", example = "10000", minimum = "1",
+                maximum = "1000000", requiredMode = Schema.RequiredMode.REQUIRED)
+        private Long amount;
+    }
+
+    record MockChargeBalance(Long balance){
+        public static MockChargeBalance dummy(){
+            return new MockChargeBalance(20_000L);
+        }
     }
 
     @Operation(summary = "계좌 잔액 조회 API", description = "userId를 통해 사용자 계좌 잔액을 조회한다.")
@@ -36,11 +57,6 @@ public class MockAccountApiController {
         return CommonResponse.success(result);
     }
 
-    record MockChargeBalance(Long balance){
-        public static MockChargeBalance dummy(){
-            return new MockChargeBalance(20_000L);
-        }
-    }
 
     record MockRetrieveAccount(Long balance){
         public static MockRetrieveAccount dummy(){
